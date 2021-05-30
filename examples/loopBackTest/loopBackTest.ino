9b6f52dcc91f47712fb4572da7086a02395b1b38
Asuki Kono <asukiaaa@gmail.com>
2021-05-30 15:04:43 +0900

    Create draft version for CanBusMCP_asukiaaa

---
+++ examples/loopBackTest/loopBackTest.ino
@@ -0,0 +1,63 @@
+#include <CanBusMCP2515_asukiaaa.h>
+#define PIN_CS 10
+#define PIN_INT 7
+
+static const uint32_t QUARTZ_FREQUENCY = 16UL * 1000UL * 1000UL;  // 16 MHz
+
+CanBusMCP2515_asukiaaa::Driver can(PIN_CS, PIN_INT);
+
+void setup() {
+  CanBusMCP2515_asukiaaa::Settings settings(
+      QUARTZ_FREQUENCY,
+      125UL * 1000UL);  // CAN bit rate 125 kb/s
+  settings.mRequestedMode =
+      CanBusMCP2515_asukiaaa::Settings::LoopBackMode;  // Select loopback mode
+  const uint16_t errorCode = can.begin(settings, [] { can.isr(); });
+  if (errorCode == 0) {
+    Serial.print("Bit Rate prescaler: ");
+    Serial.println(settings.mBitRatePrescaler);
+    Serial.print("Propagation Segment: ");
+    Serial.println(settings.mPropagationSegment);
+    Serial.print("Phase segment 1: ");
+    Serial.println(settings.mPhaseSegment1);
+    Serial.print("Phase segment 2: ");
+    Serial.println(settings.mPhaseSegment2);
+    Serial.print("SJW: ");
+    Serial.println(settings.mSJW);
+    Serial.print("Triple Sampling: ");
+    Serial.println(settings.mTripleSampling ? "yes" : "no");
+    Serial.print("Actual bit rate: ");
+    Serial.print(settings.actualBitRate());
+    Serial.println(" bit/s");
+    Serial.print("Exact bit rate ? ");
+    Serial.println(settings.exactBitRate() ? "yes" : "no");
+    Serial.print("Sample point: ");
+    Serial.print(settings.samplePointFromBitStart());
+    Serial.println("%");
+  } else {
+    Serial.print("Configuration error 0x");
+    Serial.println(errorCode, HEX);
+  }
+}
+
+void loop() {
+  CanBusData_asukiaaa::Frame frame;
+  static unsigned long trySendAt = 0;
+  static const unsigned long intervalMs = 1000UL;
+  if (trySendAt == 0 || millis() - trySendAt > intervalMs) {
+    trySendAt = millis();
+    const bool ok = can.tryToSend(frame);
+    if (ok) {
+      Serial.print("SentAt: ");
+      Serial.println(millis());
+    } else {
+      Serial.print("Send failure at ");
+      Serial.println(millis());
+    }
+  }
+  if (can.available()) {
+    can.receive(frame);
+    Serial.print("ReceivedAt: ");
+    Serial.println(millis());
+  }
+}
---
+++ src/CanBusMCP2515_asukiaaa/Driver.h
@@ -0,0 +1,148 @@
+#pragma once
+
+#include <Arduino.h>
+#include <SPI.h>
+
+#include <CanBusData_asukiaaa.hpp>
+
+#include "Settings.h"
+#include "ACANBuffer16.h"
+#include "MCP2515ReceiveFilters.h"
+
+// #define SERIAL_DEBUG Serial
+
+namespace CanBusMCP2515_asukiaaa {
+
+typedef void (*ACANCallBackRoutine)(
+    const CanBusData_asukiaaa::Frame &inMessage);
+
+class Driver {
+ public:
+  Driver(const uint8_t inCS,    // CS input of MCP2515
+         const uint8_t inINT);  // INT output of MCP2515
+  uint16_t begin(const Settings &inSettings,
+                 void (*inInterruptServiceRoutine)(void));
+  uint16_t begin(const Settings &inSettings,
+                 void (*inInterruptServiceRoutine)(void),
+                 const ACAN2515Mask inRXM0,
+                 const ACAN2515AcceptanceFilter inAcceptanceFilters[],
+                 const uint8_t inAcceptanceFilterCount);
+  uint16_t begin(const Settings &inSettings,
+                 void (*inInterruptServiceRoutine)(void),
+                 const ACAN2515Mask inRXM0, const ACAN2515Mask inRXM1,
+                 const ACAN2515AcceptanceFilter inAcceptanceFilters[],
+                 const uint8_t inAcceptanceFilterCount);
+  static const uint16_t kNoMCP2515 = 1 << 0;
+  static const uint16_t kTooFarFromDesiredBitRate = 1 << 1;
+  static const uint16_t kInconsistentBitRateSettings = 1 << 2;
+  static const uint16_t kINTPinIsNotAnInterrupt = 1 << 3;
+  static const uint16_t kISRIsNull = 1 << 4;
+  static const uint16_t kRequestedModeTimeOut = 1 << 5;
+  static const uint16_t kAcceptanceFilterArrayIsNULL = 1 << 6;
+  static const uint16_t kOneFilterMaskRequiresOneOrTwoAcceptanceFilters = 1
+                                                                          << 7;
+  static const uint16_t kTwoFilterMasksRequireThreeToSixAcceptanceFilters =
+      1 << 8;
+  static const uint16_t kCannotAllocateReceiveBuffer = 1 << 9;
+  static const uint16_t kCannotAllocateTransmitBuffer0 = 1 << 10;
+  static const uint16_t kCannotAllocateTransmitBuffer1 = 1 << 11;
+  static const uint16_t kCannotAllocateTransmitBuffer2 = 1 << 12;
+  static const uint32_t kISRNotNullAndNoIntPin = 1 << 13;
+  uint16_t changeModeOnTheFly(
+      const Settings::RequestedMode inRequestedMode);
+  uint16_t setFiltersOnTheFly(void);
+  uint16_t setFiltersOnTheFly(
+      const ACAN2515Mask inRXM0,
+      const ACAN2515AcceptanceFilter inAcceptanceFilters[],
+      const uint8_t inAcceptanceFilterCount);
+  uint16_t setFiltersOnTheFly(
+      const ACAN2515Mask inRXM0, const ACAN2515Mask inRXM1,
+      const ACAN2515AcceptanceFilter inAcceptanceFilters[],
+      const uint8_t inAcceptanceFilterCount);
+  void end(void);
+  bool available(void);
+  bool receive(CanBusData_asukiaaa::Frame &outFrame);
+  typedef void (*tFilterMatchCallBack)(const uint8_t inFilterIndex);
+  bool dispatchReceivedMessage(
+      const tFilterMatchCallBack inFilterMatchCallBack = NULL);
+  void isr(void);
+  bool isr_core(void);
+
+#ifdef ARDUINO_ARCH_ESP32
+  SemaphoreHandle_t mISRSemaphore;
+#endif
+
+  inline uint16_t receiveBufferSize(void) const {
+    return mReceiveBuffer.size();
+  }
+
+  inline uint16_t receiveBufferCount(void) const {
+    return mReceiveBuffer.count();
+  }
+
+  inline uint16_t receiveBufferPeakCount(void) const {
+    return mReceiveBuffer.peakCount();
+  }
+
+  bool tryToSend(const CanBusData_asukiaaa::Frame &inMessage);
+
+  inline uint16_t transmitBufferSize(const uint8_t inIndex) const {
+    return mTransmitBuffer[inIndex].size();
+  }
+  inline uint16_t transmitBufferCount(const uint8_t inIndex) const {
+    return mTransmitBuffer[inIndex].count();
+  }
+  inline uint16_t transmitBufferPeakCount(const uint8_t inIndex) const {
+    return mTransmitBuffer[inIndex].peakCount();
+  }
+
+  void poll(void);
+
+  uint8_t receiveErrorCounter(void);
+  uint8_t transmitErrorCounter(void);
+  uint8_t errorFlagRegister(void);
+
+ private:
+  ACANBuffer16 mTransmitBuffer[3];
+  bool mTXBIsFree[3];
+  ACANCallBackRoutine mCallBackFunctionArray[6];
+  ACANBuffer16 mReceiveBuffer;
+  void handleTXBInterrupt(const uint8_t inTXB);
+  void handleRXBInterrupt(void);
+  SPIClass* mSpi = NULL;
+  const SPISettings mSPISettings;
+  const uint8_t mCS;
+  const uint8_t mINT;
+
+  void internalSendMessage(const CanBusData_asukiaaa::Frame &inFrame,
+                           const uint8_t inTXB);
+  inline void select(void) { digitalWrite(mCS, LOW); }
+  inline void unselect(void) { digitalWrite(mCS, HIGH); }
+  uint16_t beginWithoutFilterCheck(
+      const Settings &inSettings,
+      void (*inInterruptServiceRoutine)(void), const ACAN2515Mask inRXM0,
+      const ACAN2515Mask inRXM1,
+      const ACAN2515AcceptanceFilter inAcceptanceFilters[],
+      const uint8_t inAcceptanceFilterCount);
+  uint16_t internalBeginOperation(
+      const Settings &inSettings, const ACAN2515Mask inRXM0,
+      const ACAN2515Mask inRXM1,
+      const ACAN2515AcceptanceFilter inAcceptanceFilters[],
+      const uint8_t inAcceptanceFilterCount);
+  void write2515Register(const uint8_t inRegister, const uint8_t inValue);
+  uint8_t read2515Register(const uint8_t inRegister);
+  uint8_t read2515Status(void);
+  uint8_t read2515RxStatus(void);
+  void bitModify2515Register(const uint8_t inRegister, const uint8_t inMask,
+                             const uint8_t inData);
+  void setupMaskRegister(const ACAN2515Mask inMask, const uint8_t inRegister);
+  uint16_t setRequestedMode(const uint8_t inCANControlRegister);
+  uint16_t internalSetFiltersOnTheFly(
+      const ACAN2515Mask inRXM0, const ACAN2515Mask inRXM1,
+      const ACAN2515AcceptanceFilter inAcceptanceFilters[],
+      const uint8_t inAcceptanceFilterCount);
+  Driver(const Driver &) = delete;
+  Driver &operator=(const Driver &) = delete;
+};
+
+};  // namespace CanBusMCP2515_asukiaaa
